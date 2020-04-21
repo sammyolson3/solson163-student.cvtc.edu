56fb0d7ea1c0cfdd6fe284c6d8ce3f39a75c677c
Meghan Walley <meghanr.walley@gmail.com>
2020-04-21 11:07:02 -0500

    style changes added grid.js

---
+++ artGallery/js/grid.js
@@ -0,0 +1,17 @@
+//Get the button
+var upBtn = document.getElementById("upBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    upBtn.style.display = "block";
+  } else {
+    upBtn.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
