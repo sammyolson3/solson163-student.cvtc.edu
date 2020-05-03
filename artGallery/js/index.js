e003d69e8f1705febde29f983d2960b7d8b72f06
Samantha Olson <solson163@student.cvtc.edu>
2020-05-02 21:30:45 -0500

    changes...

--- artGallery/js/index.js
+++ artGallery/js/index.js
@@ -4,4 +4,9 @@ $(function() {
         $('.flyInText').removeClass('hidden');
     }, 500);
 
-})();
+	$('#navBtn').click(function() {
+        var toggleWidth = $('#navContent').width() == 650 ? "0" : "650px";
+		$('#navContent').animate({width: toggleWidth }, 1000);
+	});
+
+});
