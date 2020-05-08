a507fa0b66097b2a4aff6f22d79d6d0c2de0db3e
Samantha Olson <solson163@student.cvtc.edu>
2020-05-07 20:36:39 -0500

    added and X to the open Nav

--- artGallery/js/main.js
+++ artGallery/js/main.js
@@ -1,3 +1,9 @@
+$(document).ready(function(){
+	$('.navBtn').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
 function show() {
     document.getElementById('sideNav').classList.toggle('active');
 }
