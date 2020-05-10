f6c79df8d36a2de55039d811e03d36c4af18118d
Meghan Walley <meghanr.walley@gmail.com>
2020-05-10 17:04:50 -0500

    changes and fixed errors

--- artGallery/js/grid.js
+++ artGallery/js/grid.js
@@ -1,17 +1,6 @@
 //	Get the upBtn and scroll to top
 var upBtn = document.getElementById('upBtn');
 
-window.onscroll = function () {
-	scrollFunction();
-};
-
-function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		upBtn.style.display = 'block';
-	} else {
-		upBtn.style.display = 'none';
-	}
-}
 function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
